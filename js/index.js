7f4600624d91f054adfa209dd7c02339fe3f12ba
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-11-25 00:53:24 +0300

    feat: implemented header

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const headerHamburger = document.querySelector('.header__hamburger');
+
+const headerCloseButton = document.querySelector('.header__nav-close-button');
+
+const headerNav = document.querySelector('.header__nav');
+
+headerHamburger.addEventListener('click', () => {
+  headerNav.classList.toggle('active');
+});
+
+headerCloseButton.addEventListener('click', () => {
+  headerNav.classList.remove('active');
+});
+
+// headerNav.addEventListener('click', (event) => {
+//   const isCurrentTarget = event.target.classList.contains('.header__nav');
+//   if (!isCurrentTarget) headerNav.remove('active');
+//   console.log(isCurrentTarget);
+// });
