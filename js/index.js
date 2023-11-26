4322d76a9029d7609aed66e5c435cba9552aeae5
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-11-26 17:58:45 +0300

    fix: fixed menu & cart issues

--- js/index.js
+++ js/index.js
@@ -4,12 +4,21 @@ const headerCloseButton = document.querySelector('.header__nav-close-button');
 
 const headerNav = document.querySelector('.header__nav');
 
+const headerLayout = document.querySelector('.header__layout');
+
 headerHamburger.addEventListener('click', () => {
   headerNav.classList.toggle('active');
+  headerLayout.classList.toggle('active');
 });
 
 headerCloseButton.addEventListener('click', () => {
   headerNav.classList.remove('active');
+  headerLayout.classList.remove('active');
+});
+
+headerLayout.addEventListener('click', () => {
+  headerNav.classList.remove('active');
+  headerLayout.classList.remove('active');
 });
 
 const mainSwiper = new Swiper('.main-slider .swiper', {
